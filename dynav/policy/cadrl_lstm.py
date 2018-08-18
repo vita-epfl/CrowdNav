3304ccba52a0b6649bfd9a82e57e51377a8b3323
Changan Chen <changanvr@gmail.com>
2018-08-18 11:19:07 +0200

    switch to local coordinate frame

--- dynav/policy/cadrl_lstm.py
+++ dynav/policy/cadrl_lstm.py
@@ -10,12 +10,11 @@ from dynav.policy.cadrl import CADRL
 class ValueNetwork(nn.Module):
     def __init__(self, input_dim, kinematics, mlp_dims, lstm_hidden_dim):
         super().__init__()
-        self.input_dim = input_dim
         self.kinematics = kinematics
-        self.mlp = nn.Sequential(nn.Linear(26, mlp_dims[0]), nn.ReLU(),
+        self.mlp = nn.Sequential(nn.Linear(input_dim, mlp_dims[0]), nn.ReLU(),
                                  nn.Linear(mlp_dims[0], mlp_dims[1]), nn.ReLU(),
                                  nn.Linear(mlp_dims[1], 1))
-        self.lstm = nn.LSTM(9, lstm_hidden_dim, batch_first=True)
+        self.lstm = nn.LSTM(7, lstm_hidden_dim, batch_first=True)
 
     def rotate(self, state):
         """
@@ -40,21 +39,19 @@ class ValueNetwork(nn.Module):
             theta = (state[:, 8] - rot).reshape((batch, -1))
         else:
             theta = (state[:, 8]).reshape((batch, -1))
-        vx1 = (state[:, 11] * torch.cos(rot) + state[:, 12] * torch.sin(rot)).reshape((batch, -1))
-        vy1 = (state[:, 12] * torch.cos(rot) - state[:, 11] * torch.sin(rot)).reshape((batch, -1))
+        vx1 = (state[:, 11] * torch.cos(rot) + state[:, 12] * torch.sin(rot)).reshape((batch, -1)) - vx
+        vy1 = (state[:, 12] * torch.cos(rot) - state[:, 11] * torch.sin(rot)).reshape((batch, -1)) - vy
         px1 = (state[:, 9] - state[:, 0]) * torch.cos(rot) + (state[:, 10] - state[:, 1]) * torch.sin(rot)
         px1 = px1.reshape((batch, -1))
         py1 = (state[:, 10] - state[:, 1]) * torch.cos(rot) - (state[:, 9] - state[:, 0]) * torch.sin(rot)
         py1 = py1.reshape((batch, -1))
         radius1 = state[:, 13].reshape((batch, -1))
         radius_sum = radius + radius1
-        cos_theta = torch.cos(theta)
-        sin_theta = torch.sin(theta)
         da = torch.norm(torch.cat([(state[:, 0] - state[:, 9]).reshape((batch, -1)), (state[:, 1] - state[:, 10]).
                                   reshape((batch, -1))], dim=1), 2, dim=1, keepdim=True)
 
-        self_state = torch.cat([dg, v_pref, vx, vy, radius, theta], dim=1)
-        ped_state = torch.cat([vx1, vy1, px1, py1, radius1, radius_sum, cos_theta, sin_theta, da], dim=1)
+        self_state = torch.cat([dg, v_pref, theta, radius], dim=1)
+        ped_state = torch.cat([px1, py1, vx1, vy1, radius1, da, radius_sum], dim=1)
 
         return self_state, ped_state
 
