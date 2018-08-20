store transformed state in the experience set and correct value update in one-step lookahead

dynav/policy/cadrl.py
@@ -10,55 +10,15 @@ from dynav.policy.utils import reward
 
 
 class ValueNetwork(nn.Module):
-    def __init__(self, input_dim, kinematics, mlp_dims):
+    def __init__(self, input_dim, mlp_dims):
         super().__init__()
         self.input_dim = input_dim
-        self.kinematics = kinematics
         self.value_network = nn.Sequential(nn.Linear(input_dim, mlp_dims[0]), nn.ReLU(),
                                            nn.Linear(mlp_dims[0], mlp_dims[1]), nn.ReLU(),
                                            nn.Linear(mlp_dims[1], mlp_dims[2]), nn.ReLU(),
                                            nn.Linear(mlp_dims[2], 1))
 
-    @staticmethod
-    def rotate(state, kinematics):
-        """
-        Input state tensor is of size (batch_size, state_length)
-
-        """
-        # 'px', 'py', 'vx', 'vy', 'radius', 'gx', 'gy', 'v_pref', 'theta', 'px1', 'py1', 'vx1', 'vy1', 'radius1'
-        #  0     1      2     3      4        5     6      7         8       9     10      11     12       13
-        batch = state.shape[0]
-        dx = (state[:, 5] - state[:, 0]).reshape((batch, -1))
-        dy = (state[:, 6] - state[:, 1]).reshape((batch, -1))
-        rot = torch.atan2(state[:, 6] - state[:, 1], state[:, 5] - state[:, 0])
-
-        dg = torch.norm(torch.cat([dx, dy], dim=1), 2, dim=1, keepdim=True)
-        v_pref = state[:, 7].reshape((batch, -1))
-        vx = (state[:, 2] * torch.cos(rot) + state[:, 3] * torch.sin(rot)).reshape((batch, -1))
-        vy = (state[:, 3] * torch.cos(rot) - state[:, 2] * torch.sin(rot)).reshape((batch, -1))
-
-        radius = state[:, 4].reshape((batch, -1))
-        if kinematics == 'unicycle':
-            theta = (state[:, 8] - rot).reshape((batch, -1))
-        else:
-            theta = (state[:, 8]).reshape((batch, -1))
-        vx1 = (state[:, 11] * torch.cos(rot) + state[:, 12] * torch.sin(rot)).reshape((batch, -1)) - vx
-        vy1 = (state[:, 12] * torch.cos(rot) - state[:, 11] * torch.sin(rot)).reshape((batch, -1)) - vy
-        px1 = (state[:, 9] - state[:, 0]) * torch.cos(rot) + (state[:, 10] - state[:, 1]) * torch.sin(rot)
-        px1 = px1.reshape((batch, -1))
-        py1 = (state[:, 10] - state[:, 1]) * torch.cos(rot) - (state[:, 9] - state[:, 0]) * torch.sin(rot)
-        py1 = py1.reshape((batch, -1))
-        radius1 = state[:, 13].reshape((batch, -1))
-        radius_sum = radius + radius1
-        da = torch.norm(torch.cat([(state[:, 0] - state[:, 9]).reshape((batch, -1)), (state[:, 1] - state[:, 10]).
-                                  reshape((batch, -1))], dim=1), 2, dim=1, keepdim=True)
-
-        new_state = torch.cat([dg, v_pref, theta, radius, px1, py1, vx1, vy1, radius1, da, radius_sum], dim=1)
-        return new_state
-
     def forward(self, state):
-        # transform the world coordinates to self-centric coordinates
-        state = self.rotate(state, self.kinematics)
         value = self.value_network(state)
         return value
 
@@ -75,20 +35,21 @@ class CADRL(Policy):
         self.speed_samples = None
         self.rotation_samples = None
         self.action_space = None
+        self.joint_state_dim = 11
 
     def configure(self, config):
-        self.gamma = config.getfloat('rl', 'gamma')
+        self.set_common_parameters(config)
+        mlp_dims = [int(x) for x in config.get('cadrl', 'mlp_dims').split(', ')]
+        self.model = ValueNetwork(self.joint_state_dim, mlp_dims)
+        self.multiagent_training = config.getboolean('cadrl', 'multiagent_training')
+        logging.info('CADRL: {} agent training'.format('single' if not self.multiagent_training else 'multiple'))
 
+    def set_common_parameters(self, config):
+        self.gamma = config.getfloat('rl', 'gamma')
         self.kinematics = config.get('action_space', 'kinematics')
         self.sampling = config.get('action_space', 'sampling')
         self.speed_samples = config.getint('action_space', 'speed_samples')
         self.rotation_samples = config.getint('action_space', 'rotation_samples')
-
-        input_dim = config.getint('cadrl', 'input_dim')
-        mlp_dims = [int(x) for x in config.get('cadrl', 'mlp_dims').split(', ')]
-        self.model = ValueNetwork(input_dim, self.kinematics, mlp_dims)
-        self.multiagent_training = config.getboolean('cadrl', 'multiagent_training')
-        logging.info('CADRL: {} agent training'.format('single' if not self.multiagent_training else 'multiple'))
 
     def set_device(self, device):
         self.device = device
@@ -168,7 +129,7 @@ class CADRL(Policy):
             raise AttributeError('Epsilon attribute has to be set in training phase')
 
         if self.reach_destination(state):
-            return ActionXY(0, 0)
+            return ActionXY(0, 0) if self.kinematics == 'holonomic' else ActionRot(0, 0)
         self.build_action_space(state.self_state.v_pref)
 
         probability = np.random.random()
@@ -185,10 +146,11 @@ class CADRL(Policy):
                     next_dual_state = torch.Tensor([next_self_state + next_ped_state]).to(self.device)
                     batch_next_states.append(next_dual_state)
                 batch_next_states = torch.cat(batch_next_states, dim=0)
-                outputs = self.model(batch_next_states)
+                # VALUE UPDATE
+                outputs = self.model(self.rotate(batch_next_states))
                 min_output, min_index = torch.min(outputs, 0)
-                min_value = reward(state, action, self.kinematics, self.time_step) + \
-                    pow(self.gamma, state.self_state.v_pref) * min_output.data.item()
+                gamma_bar = pow(self.gamma, self.time_step * state.self_state.v_pref)
+                min_value = reward(state, action, self.kinematics, self.time_step) + gamma_bar * min_output.data.item()
                 if min_value > max_min_value:
                     max_min_value = min_value
                     max_action = action
@@ -206,4 +168,43 @@ class CADRL(Policy):
         :return: tensor of shape (len(state), )
         """
         assert len(state.ped_states) == 1
-        return torch.Tensor(state.self_state + state.ped_states[0]).to(self.device)
+        state = torch.Tensor(state.self_state + state.ped_states[0]).to(self.device)
+        state = self.rotate(state.unsqueeze(0)).squeeze(dim=0)
+        return state
+
+    def rotate(self, state):
+        """
+        Transform the coordinate to agent-centric.
+        Input state tensor is of size (batch_size, state_length)
+
+        """
+        # 'px', 'py', 'vx', 'vy', 'radius', 'gx', 'gy', 'v_pref', 'theta', 'px1', 'py1', 'vx1', 'vy1', 'radius1'
+        #  0     1      2     3      4        5     6      7         8       9     10      11     12       13
+        batch = state.shape[0]
+        dx = (state[:, 5] - state[:, 0]).reshape((batch, -1))
+        dy = (state[:, 6] - state[:, 1]).reshape((batch, -1))
+        rot = torch.atan2(state[:, 6] - state[:, 1], state[:, 5] - state[:, 0])
+
+        dg = torch.norm(torch.cat([dx, dy], dim=1), 2, dim=1, keepdim=True)
+        v_pref = state[:, 7].reshape((batch, -1))
+        vx = (state[:, 2] * torch.cos(rot) + state[:, 3] * torch.sin(rot)).reshape((batch, -1))
+        vy = (state[:, 3] * torch.cos(rot) - state[:, 2] * torch.sin(rot)).reshape((batch, -1))
+
+        radius = state[:, 4].reshape((batch, -1))
+        if self.kinematics == 'unicycle':
+            theta = (state[:, 8] - rot).reshape((batch, -1))
+        else:
+            # set theta to be zero since it's not used
+            theta = torch.zeros_like(v_pref)
+        vx1 = (state[:, 11] * torch.cos(rot) + state[:, 12] * torch.sin(rot)).reshape((batch, -1)) - vx
+        vy1 = (state[:, 12] * torch.cos(rot) - state[:, 11] * torch.sin(rot)).reshape((batch, -1)) - vy
+        px1 = (state[:, 9] - state[:, 0]) * torch.cos(rot) + (state[:, 10] - state[:, 1]) * torch.sin(rot)
+        px1 = px1.reshape((batch, -1))
+        py1 = (state[:, 10] - state[:, 1]) * torch.cos(rot) - (state[:, 9] - state[:, 0]) * torch.sin(rot)
+        py1 = py1.reshape((batch, -1))
+        radius1 = state[:, 13].reshape((batch, -1))
+        radius_sum = radius + radius1
+        da = torch.norm(torch.cat([(state[:, 0] - state[:, 9]).reshape((batch, -1)), (state[:, 1] - state[:, 10]).
+                                  reshape((batch, -1))], dim=1), 2, dim=1, keepdim=True)
+        new_state = torch.cat([dg, v_pref, theta, radius, px1, py1, vx1, vy1, radius1, da, radius_sum], dim=1)
+        return new_state

dynav/policy/cadrl_lstm.py
@@ -8,52 +8,12 @@ from dynav.policy.cadrl import CADRL
 
 
 class ValueNetwork(nn.Module):
-    def __init__(self, input_dim, kinematics, mlp_dims, lstm_hidden_dim):
+    def __init__(self, input_dim, mlp_dims, lstm_input_dim, lstm_hidden_dim):
         super().__init__()
-        self.kinematics = kinematics
         self.mlp = nn.Sequential(nn.Linear(input_dim, mlp_dims[0]), nn.ReLU(),
                                  nn.Linear(mlp_dims[0], mlp_dims[1]), nn.ReLU(),
                                  nn.Linear(mlp_dims[1], 1))
-        self.lstm = nn.LSTM(7, lstm_hidden_dim, batch_first=True)
-
-    def rotate(self, state):
-        """
-
-        :param state: tensor of size (batch_size, # of peds, length of joint state)
-        :return:
-        """
-        # 'px', 'py', 'vx', 'vy', 'radius', 'gx', 'gy', 'v_pref', 'theta', 'px1', 'py1', 'vx1', 'vy1', 'radius1'
-        #  0     1      2     3      4        5     6      7         8       9     10      11     12       13
-        batch = state.shape[0]
-        dx = (state[:, 5] - state[:, 0]).reshape((batch, -1))
-        dy = (state[:, 6] - state[:, 1]).reshape((batch, -1))
-        rot = torch.atan2(state[:, 6] - state[:, 1], state[:, 5] - state[:, 0])
-
-        dg = torch.norm(torch.cat([dx, dy], dim=1), 2, dim=1, keepdim=True)
-        v_pref = state[:, 7].reshape((batch, -1))
-        vx = (state[:, 2] * torch.cos(rot) + state[:, 3] * torch.sin(rot)).reshape((batch, -1))
-        vy = (state[:, 3] * torch.cos(rot) - state[:, 2] * torch.sin(rot)).reshape((batch, -1))
-
-        radius = state[:, 4].reshape((batch, -1))
-        if self.kinematics == 'unicycle':
-            theta = (state[:, 8] - rot).reshape((batch, -1))
-        else:
-            theta = (state[:, 8]).reshape((batch, -1))
-        vx1 = (state[:, 11] * torch.cos(rot) + state[:, 12] * torch.sin(rot)).reshape((batch, -1)) - vx
-        vy1 = (state[:, 12] * torch.cos(rot) - state[:, 11] * torch.sin(rot)).reshape((batch, -1)) - vy
-        px1 = (state[:, 9] - state[:, 0]) * torch.cos(rot) + (state[:, 10] - state[:, 1]) * torch.sin(rot)
-        px1 = px1.reshape((batch, -1))
-        py1 = (state[:, 10] - state[:, 1]) * torch.cos(rot) - (state[:, 9] - state[:, 0]) * torch.sin(rot)
-        py1 = py1.reshape((batch, -1))
-        radius1 = state[:, 13].reshape((batch, -1))
-        radius_sum = radius + radius1
-        da = torch.norm(torch.cat([(state[:, 0] - state[:, 9]).reshape((batch, -1)), (state[:, 1] - state[:, 10]).
-                                  reshape((batch, -1))], dim=1), 2, dim=1, keepdim=True)
-
-        self_state = torch.cat([dg, v_pref, theta, radius], dim=1)
-        ped_state = torch.cat([px1, py1, vx1, vy1, radius1, da, radius_sum], dim=1)
-
-        return self_state, ped_state
+        self.lstm = nn.LSTM(lstm_input_dim, lstm_hidden_dim, batch_first=True)
 
     def forward(self, state):
         """
@@ -63,9 +23,8 @@ class ValueNetwork(nn.Module):
         :return:
         """
         size = state.shape
-        self_state, ped_state = self.rotate(torch.reshape(state, (-1, size[2])))
-        self_state = torch.reshape(self_state, (size[0], size[1], -1))[:, 0, :]
-        ped_state = torch.reshape(ped_state, (size[0], size[1], -1))
+        self_state = state[:, 0, :4]
+        ped_state = state[:, :, 4:]
         h0 = torch.zeros(1, size[0], 20)
         c0 = torch.zeros(1, size[0], 20)
         output, (hn, cn) = self.lstm(ped_state, (h0, c0))
@@ -80,17 +39,12 @@ class CadrlLSTM(CADRL):
         super().__init__()
 
     def configure(self, config):
-        self.gamma = config.getfloat('rl', 'gamma')
-
-        self.kinematics = config.get('action_space', 'kinematics')
-        self.sampling = config.get('action_space', 'sampling')
-        self.speed_samples = config.getint('action_space', 'speed_samples')
-        self.rotation_samples = config.getint('action_space', 'rotation_samples')
-
-        input_dim = config.getint('cadrl_lstm', 'input_dim')
+        self.set_common_parameters(config)
+        self.joint_state_dim = 24
         mlp_dims = [int(x) for x in config.get('cadrl_lstm', 'mlp_dims').split(', ')]
         lstm_hidden_dim = config.getint('cadrl_lstm', 'lstm_hidden_dim')
-        self.model = ValueNetwork(input_dim, self.kinematics, mlp_dims, lstm_hidden_dim)
+        lstm_input_dim = 7
+        self.model = ValueNetwork(self.joint_state_dim, mlp_dims, lstm_input_dim, lstm_hidden_dim)
         self.multiagent_training = config.getboolean('cadrl_lstm', 'multiagent_training')
         logging.info('LSTM: {} agent training'.format('single' if not self.multiagent_training else 'multiple'))
 
@@ -108,7 +62,7 @@ class CadrlLSTM(CADRL):
             raise AttributeError('Epsilon attribute has to be set in training phase')
 
         if self.reach_destination(state):
-            return ActionXY(0, 0)
+            return ActionXY(0, 0) if self.kinematics == 'holonomic' else ActionRot(0, 0)
         self.build_action_space(state.self_state.v_pref)
 
         probability = np.random.random()
@@ -129,9 +83,11 @@ class CadrlLSTM(CADRL):
                     next_ped_state = self.propagate(ped_state, ActionXY(ped_state.vx, ped_state.vy))
                     next_dual_state = torch.Tensor([next_self_state + next_ped_state]).to(self.device)
                     batch_next_states.append(next_dual_state)
-                batch_next_states = torch.cat(batch_next_states, dim=0).unsqueeze(0)
-                value = reward(state, action, self.kinematics, self.time_step) + \
-                    pow(self.gamma, state.self_state.v_pref) * self.model(batch_next_states).data.item()
+                batch_next_states = torch.cat(batch_next_states, dim=0)
+                # VALUE UPDATE
+                next_state_value = self.model(self.rotate(batch_next_states).unsqueeze(0)).data.item()
+                gamma_bar = pow(self.gamma, self.time_step * state.self_state.v_pref)
+                value = reward(state, action, self.kinematics, self.time_step) + gamma_bar * next_state_value
                 if value > max_value:
                     max_value = value
                     max_action = action
@@ -148,5 +104,7 @@ class CadrlLSTM(CADRL):
         :param state:
         :return: tensor of shape (len(state), )
         """
-        return torch.cat([torch.Tensor([state.self_state + ped_state]).to(self.device)
+        state = torch.cat([torch.Tensor([state.self_state + ped_state]).to(self.device)
                           for ped_state in state.ped_states], dim=0)
+        state = self.rotate(state)
+        return state

dynav/policy/srl.py
@@ -4,14 +4,13 @@ import numpy as np
 import logging
 from gym_crowd.envs.utils.action import ActionRot, ActionXY
 from dynav.policy.utils import reward
-from dynav.policy.cadrl import CADRL, ValueNetwork as VN
+from dynav.policy.cadrl import CADRL
 
 
 class ValueNetwork(nn.Module):
-    def __init__(self, input_dim, kinematics, mlp1_dims, mlp2_dims):
+    def __init__(self, input_dim, mlp1_dims, mlp2_dims):
         super().__init__()
         self.input_dim = input_dim
-        self.kinematics = kinematics
         self.mlp1 = nn.Sequential(nn.Linear(input_dim, mlp1_dims[0]), nn.ReLU(),
                                   nn.Linear(mlp1_dims[0], mlp1_dims[1]), nn.ReLU(),
                                   nn.Linear(mlp1_dims[1], mlp1_dims[2]), nn.ReLU(),
@@ -22,11 +21,11 @@ class ValueNetwork(nn.Module):
         """
         First transform the world coordinates to self-centric coordinates and then do forward computation
 
-        :param state: tensor of shape (batch_size, # of peds, length of a joint state)
+        :param state: tensor of shape (batch_size, # of peds, length of a rotated state)
         :return:
         """
         size = state.shape
-        state = VN.rotate(torch.reshape(state, (-1, size[2])), self.kinematics)
+        state = torch.reshape(state, (-1, size[2]))
         output = torch.reshape(self.mlp1(state), (size[0], size[1], -1))
         value = self.mlp2(torch.max(output, 1)[0])
         return value
@@ -37,17 +36,10 @@ class SRL(CADRL):
         super().__init__()
 
     def configure(self, config):
-        self.gamma = config.getfloat('rl', 'gamma')
-
-        self.kinematics = config.get('action_space', 'kinematics')
-        self.sampling = config.get('action_space', 'sampling')
-        self.speed_samples = config.getint('action_space', 'speed_samples')
-        self.rotation_samples = config.getint('action_space', 'rotation_samples')
-
-        input_dim = config.getint('srl', 'input_dim')
+        self.set_common_parameters(config)
         mlp1_dims = [int(x) for x in config.get('srl', 'mlp1_dims').split(', ')]
         mlp2_dims = config.getint('srl', 'mlp2_dims')
-        self.model = ValueNetwork(input_dim, self.kinematics, mlp1_dims, mlp2_dims)
+        self.model = ValueNetwork(self.joint_state_dim, mlp1_dims, mlp2_dims)
         self.multiagent_training = config.getboolean('srl', 'multiagent_training')
         logging.info('SRL: {} agent training'.format('single' if not self.multiagent_training else 'multiple'))
 
@@ -65,7 +57,7 @@ class SRL(CADRL):
             raise AttributeError('Epsilon attribute has to be set in training phase')
 
         if self.reach_destination(state):
-            return ActionXY(0, 0)
+            return ActionXY(0, 0) if self.kinematics == 'holonomic' else ActionRot(0, 0)
         self.build_action_space(state.self_state.v_pref)
 
         probability = np.random.random()
@@ -81,9 +73,11 @@ class SRL(CADRL):
                     next_ped_state = self.propagate(ped_state, ActionXY(ped_state.vx, ped_state.vy))
                     next_dual_state = torch.Tensor([next_self_state + next_ped_state]).to(self.device)
                     batch_next_states.append(next_dual_state)
-                batch_next_states = torch.cat(batch_next_states, dim=0).unsqueeze(0)
-                value = reward(state, action, self.kinematics, self.time_step) + \
-                    pow(self.gamma, state.self_state.v_pref) * self.model(batch_next_states).data.item()
+                batch_next_states = torch.cat(batch_next_states, dim=0)
+                # VALUE UPDATE
+                next_state_value = self.model(self.rotate(batch_next_states).unsqueeze(0)).data.item()
+                gamma_bar = pow(self.gamma, self.time_step * state.self_state.v_pref)
+                value = reward(state, action, self.kinematics, self.time_step) + gamma_bar * next_state_value
                 if value > max_value:
                     max_value = value
                     max_action = action
@@ -100,5 +94,7 @@ class SRL(CADRL):
         :param state:
         :return: tensor of shape (# of peds, len(state))
         """
-        return torch.cat([torch.Tensor([state.self_state + ped_state]).to(self.device)
+        state = torch.cat([torch.Tensor([state.self_state + ped_state]).to(self.device)
                           for ped_state in state.ped_states], dim=0)
+        state = self.rotate(state)
+        return state

dynav/train.py
@@ -108,7 +108,6 @@ def main():
         il_policy.safety_space = safety_space
         navigator.set_policy(il_policy)
         explorer.run_k_episodes(il_episodes, 'train', update_memory=True, imitation_learning=True)
-        navigator.set_policy(policy)
         trainer.optimize_batch(il_epochs)
         torch.save(model.state_dict(), il_weight_file)
         logging.info('Finish imitation learning. Weights saved.')
@@ -116,6 +115,7 @@ def main():
     explorer.update_stabilized_model(model)
 
     # reinforcement learning
+    navigator.set_policy(policy)
     navigator.print_info()
     episode = 0
     while episode < train_episodes: