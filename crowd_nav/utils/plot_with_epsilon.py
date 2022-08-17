6e35f5f21f831fbb8cce09c4f6c62164bd88db88
Kagan <kkaytekin@gmail.com>
2022-08-17 16:29:06 +0200

    RGB configuration for final benchmark

--- crowd_nav/utils/plot_with_epsilon.py
+++ crowd_nav/utils/plot_with_epsilon.py
@@ -15,7 +15,7 @@ def main():
 
     # define the names of the models you want to plot and the longest episodes you want to show
     #models = ['LSTM-RL', 'SARL', 'OM-SARL']
-    models = ['Continuing', 'Slowing', 'Expanding','Expanding & Moving']
+    models = ['SARL', 'Slowing', 'Expanding','Expanding & Moving']
     max_episodes = 200000
 
     ax1 = ax2 = ax3 = ax4 = ax5 = None
